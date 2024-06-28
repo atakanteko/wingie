8fc0d3ffce83a9f539b1fd8dfc754d608be76b51
atakanteko <atakantekoglu@gmail.com>
2024-06-28 15:58:41 +0300

    feat: Sentry to track app's behavior

---
+++ sentry.server.config.ts
@@ -0,0 +1,19 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://02f8e0b44fd6817ecb76688423fcc4d7@o4507509615951872.ingest.de.sentry.io/4507509620080720",
+
+  // Adjust this value in production, or use tracesSampler for greater control
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+
+  // Uncomment the line below to enable Spotlight (https://spotlightjs.com)
+  // spotlight: process.env.NODE_ENV === 'development',
+  
+});
